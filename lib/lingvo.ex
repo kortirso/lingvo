840e0a30f98fe1d547179aeefcd9b0b641a24400
kortirso <kortirso@gmail.com>
2018-12-21 14:49:26 +0200

    add method Lingvo.translate

--- lib/lingvo.ex
+++ lib/lingvo.ex
@@ -9,45 +9,93 @@ defmodule Lingvo do
 
   """
 
-  @type api_key :: {:api_key, String.t()}
-  @type path :: String.t()
+  alias Lingvo.Client
+
+  @type api_key :: String.t()
+
+  @available_locales [ch: 1028, da: 1030, de: 1031, el: 1032, en: 1033, es: 1034, fr: 1036, it: 1040, kk: 1087, la: 1142, pl: 1045, ru: 1049, tt: 1092, uk: 1058]
+  @available_translations ["ch-ch", "ch-ru", "da-ru", "de-ru", "el-ru", "en-en", "en-ru", "en-uk", "es-ru", "fr-ru", "it-ru", "kk-ru", "la-ru", "pl-ru", "pl-uk", "ru-ch", "ru-da", "ru-de", "ru-el", "ru-en", "ru-es", "ru-fr", "ru-it", "ru-kk", "ru-ru", "ru-tt", "ru-uk", "tt-ru", "uk-en", "uk-pl", "uk-ru", "uk-uk"]
 
   @doc """
-  Performs a GET request
+  Get API token. Token must be used in Bearer Authorization header in every translation API request.
 
   ## Examples
 
-      iex> Lingvo.get("/v1/Transalation")
-      {:ok, _}
-
-  """
-
-  def get(path) do
-    HTTPoison.get(base_uri() <> path, bearer_headers(), options())
-  end
-
-  @doc """
-  Performs a POST request
-
-  ## Examples
-
-      iex> Lingvo.post("/v1.1/authenticate")
+      iex> Lingvo.authenticate
       {:ok, "access_token"}
 
   """
-  @spec post(path) :: {:ok, api_key}
+  @spec authenticate :: {:ok, api_key}
 
-  def post(path) do
-    HTTPoison.post(base_uri() <> path, "", basic_headers(), options())
+  def authenticate do
+    Client.post("/v1.1/authenticate")
   end
 
-  defp base_uri, do: "https://developers.lingvolive.com/api"
+  @doc """
+  Translation for the word or phrase. Searches only in specified direction.
 
-  defp basic_headers, do: [{"Authorization", "Basic #{api_key()}"}, {"Accept", "Application/json; Charset=utf-8"}, {"Content-Type", "application/json"}]
+  ## Examples
 
-  defp bearer_headers, do: [{"Authorization", "Bearer #{api_key()}"}, {"Accept", "Application/json; Charset=utf-8"}, {"Content-Type", "application/json"}]
+      iex> Lingvo.translate([text: "Hola", from: "es", to: "ru"], access_token)
+      {
+        :ok,
+        %{
+          [
+            "ArticleId" => "Universal (Es-Ru)__hola",
+            ...
+          ]
+        }
+      }
 
-  defp api_key, do: Application.get_env(:lingvo, :api_key) || ""
+  ## Options
 
-  defp options, do: [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
+      text - Word or phrase to translate, required
+      from - Source language, required
+      to - Target language, required
+      case_sensitivity - If you need case-sensitive search then just add any value, optional, default is false
+
+  """
+  @spec translate([tuple], String.t()) :: String.t()
+
+  def translate(options, access_token) when is_list(options) and is_binary(access_token) do
+    [text, from, to, case_sensitivity] = read_options(options)
+    cond do
+      is_nil(text) -> {:error, "Text is required field"}
+      is_nil(from) -> {:error, "From is required field"}
+      is_nil(to) -> {:error, "To is required field"}
+      !Keyword.has_key?(@available_locales, String.to_atom(from)) -> {:error, "Source language is not available"}
+      !Keyword.has_key?(@available_locales, String.to_atom(to)) -> {:error, "Destination language is not available"}
+      !Enum.member?(@available_translations, "#{from}-#{to}") -> {:error, "Translation is not available"}
+      true -> do_translate(text, from, to, case_sensitivity, access_token)
+    end
+  end
+
+  defp read_options(options) do
+    [
+      Keyword.get(options, :text),
+      Keyword.get(options, :from),
+      Keyword.get(options, :to),
+      Keyword.get(options, :case_sensitivity)
+    ]
+  end
+
+  defp do_translate(text, from, to, case_sensitivity, access_token) do
+    url_for_translate(text, from, to, case_sensitivity)
+    |> Client.get(access_token)
+    |> parse_response()
+  end
+
+  defp url_for_translate(text, from, to, case_sensitivity) do
+    url = "/v1/Translation?text=#{text}&srcLang=#{get_code(from)}&dstLang=#{get_code(to)}"
+    if !is_nil(case_sensitivity), do: url <> "&isCaseSensitive=true", else: url
+  end
+
+  defp parse_response(response) do
+    case response do
+      {:ok, body} -> {:ok, Poison.Parser.parse!(body)}
+      {:error, body} -> {:error, body}
+    end
+  end
+
+  defp get_code(string), do: Keyword.get(@available_locales, String.to_atom(string))
 end
---
+++ lib/lingvo/client.ex
@@ -0,0 +1,60 @@
+defmodule Lingvo.Client do
+  @moduledoc """
+  Client requests
+  """
+
+  @type api_key :: {:api_key, String.t()}
+  @type path :: String.t()
+
+  @doc """
+  Performs a GET request
+
+  ## Examples
+
+      iex> Lingvo.Client.get("/v1/Transalation", access_token)
+      {:ok, %HTTPoison.Response{}}
+
+  """
+  @spec get(path, String.t()) :: %{}
+
+  def get(path, access_token) do
+    case HTTPoison.get(base_uri() <> path, bearer_headers(access_token), options()) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 400, body: body}} -> {:error, body}
+      {:ok, %HTTPoison.Response{status_code: 404}} -> {:error, "Page not found"}
+      {:error, %HTTPoison.Error{reason: reason}} -> {:error, reason}
+      _ -> {:error, "Unknown error"}
+    end
+  end
+
+  @doc """
+  Performs a POST request
+
+  ## Examples
+
+      iex> Lingvo.Client.post("/v1.1/authenticate")
+      {:ok, %HTTPoison.Response{}}
+
+  """
+  @spec post(path) :: {:ok, api_key}
+
+  def post(path) do
+    case HTTPoison.post(base_uri() <> path, "", basic_headers(), options()) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 400, body: body}} -> {:error, body}
+      {:ok, %HTTPoison.Response{status_code: 404}} -> {:error, "Page not found"}
+      {:error, %HTTPoison.Error{reason: reason}} -> {:error, reason}
+      _ -> {:error, "Unknown error"}
+    end
+  end
+
+  defp base_uri, do: "https://developers.lingvolive.com/api"
+
+  defp basic_headers, do: [{"Authorization", "Basic #{api_key()}"}, {"Accept", "Application/json; Charset=utf-8"}, {"Content-Type", "application/json"}]
+
+  defp bearer_headers(access_token), do: [{"Authorization", "Bearer #{access_token}"}, {"Accept", "Application/json; Charset=utf-8"}, {"Content-Type", "application/json"}]
+
+  defp api_key, do: Application.get_env(:lingvo, :api_key) || ""
+
+  defp options, do: [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
+end
